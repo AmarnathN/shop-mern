c2e70343c961433a472f6bb04b209b76a93818fb
AmarnathN <vayuteja@gmail.com>
2021-04-28 22:04:41 +0530

    Added ENV variables for the project

--- projbackend/app.js
+++ projbackend/app.js
@@ -2,16 +2,13 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
-mongoose
-  .connect(
-    "mongodb+srv://shop_mern_user_1:DvuSUED7IPye6iuw@shop-mern-cluster.y3uvk.mongodb.net/shop-mern-main?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
-  )
-  .then(() => {
-    console.log("DB CONNECTED");
-  });
+require("custom-env").env(process.env.NODE_ENV);
 
-const port = 8000;
+mongoose.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(() => {
+  console.log("DB CONNECTED");
+});
+
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`App is running at port : ${port} ....`);
