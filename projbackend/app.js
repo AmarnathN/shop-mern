59657cd65b06c1d3a1608e6531a5d3765c853ca6
AmarnathN <vayuteja@gmail.com>
2021-04-29 09:47:03 +0530

    Adding middlewares

--- projbackend/app.js
+++ projbackend/app.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
+// middlewares
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var cors = require("cors");
+
 require("custom-env").env(process.env.NODE_ENV);
 
 mongoose.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(() => {
@@ -9,6 +14,12 @@ mongoose.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTo
 });
 
 const port = process.env.PORT || 8000;
+
+// MIddlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`App is running at port : ${port} ....`);
