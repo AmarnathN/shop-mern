2baaf8dc809e07b39055ec5ff3ebfc4f7fb61122
AmarnathN <vayuteja@gmail.com>
2021-04-29 13:16:00 +0530

    adding express-validator for signup

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,6 +1,44 @@
 const User = require("../models/user");
+const { validationResult } = require("express-validator");
+
+exports.validatePassword = (password, { req }) => {
+  console.log(req.body);
+
+  errors = [];
+  if (password.length < 4) {
+    errors.push("Your password must be at least 4 characters.");
+  }
+  if (password.search(/[a-zA-Z]/) < 0) {
+    errors.push("Your password must contain at least one letter.");
+  }
+  if (password.search(/[0-9]/) < 0) {
+    errors.push("Your password must contain at least one digit.");
+  }
+  if (password.search(/(?=.*[!@#$%^&*])/) < 0) {
+    errors.push("Your password must contain at least one special charater in '!@#$%^&*'");
+  }
+  if (errors.length > 0) {
+    return Promise.reject(errors.join("\\n"));
+  }
+  return true;
+};
+
+exports.validateEmail = async (email) => {
+  user = await User.findOne({ email });
+  if (user) {
+    return Promise.reject("E-mail already in use");
+  }
+  return true;
+};
 
 exports.signup = (req, res) => {
+  // validationResult function checks whether
+  // any occurs or not and return an object
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json(errors);
+  }
+
   const user = new User(req.body);
   user.save((error, user) => {
     if (error) {
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { signout, signup } = require("../controllers/auth");
+const { check } = require("express-validator");
+const { signout, signup, validatePassword, validateEmail } = require("../controllers/auth");
 
-router.post("/signup", signup);
+router.post(
+  "/signup",
+  [
+    check("email").isEmail().custom(validateEmail),
+    check("password").custom(validatePassword),
+    check("name", "Name length should be min 4 characters").isLength({ min: 4 }),
+  ],
+  signup
+);
 router.get("/signout", signout);
 
 module.exports = router;
