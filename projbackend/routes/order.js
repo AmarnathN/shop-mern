452f0f110b492567d9ce104f9b8d62634e8f034e
AmarnathN <vayuteja@gmail.com>
2021-05-07 08:28:20 +0530

    Adding Middleware for updating stock and user purchase while Order is placed

--- projbackend/routes/order.js
+++ projbackend/routes/order.js
@@ -3,12 +3,13 @@ const router = express.Router();
 
 const { getOrderById, createOrder } = require("../controllers/order");
 const { isSignedIn, isAdmin } = require("../controllers/auth");
-const { getUserById } = require("../controllers/user");
+const { getUserById, pushOrderInPurchaseList } = require("../controllers/user");
+const { updateStock } = require("../controllers/product");
 
 // middleware
 router.param("userId", getUserById);
 router.param("orderId", getOrderById);
 
-router.post("/order/create/", isSignedIn, createOrder);
+router.post("/order/create/", isSignedIn, pushOrderInPurchaseList, updateStock, createOrder);
 
 module.exports = router;
