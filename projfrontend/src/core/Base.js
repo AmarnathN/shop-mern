4a859f709f4ad51770c12b3b8e7a363f5eb2933b
AmarnathN <vayuteja@gmail.com>
2021-05-18 05:27:48 +0530

    fix overflow issue

--- projfrontend/src/core/Base.js
+++ projfrontend/src/core/Base.js
@@ -2,16 +2,16 @@ import React, { Component } from "react";
 import BaseFooter from "./BaseFooter";
 import NavigationBar from "./NavigationBar";
 
-const Base = ({ title = "My Title", description = "My Description", className = "bg-dark text-white py-2", children }) => {
+const Base = ({ title = "My Title", description = "My Description", className = "bg-dark text-white p-2", children }) => {
   return (
     <div className="container-fluid bg-dark">
       <div className="card bg-dark">
-        <div class="card-header main_header">
+        <div class="card-header main_header bg-gradient">
           <NavigationBar></NavigationBar>
         </div>
-        <div class="card-body main_body">
-          <div className="bg-dark text-white text-center">
-            <h3 className="display-4">{title}</h3>
+        <div class="container-fluid card-body bg-dark main_body overflow-auto">
+          <div className=" card-title bg-dark text-white text-center">
+            <h1 className=" card-text text-warning">{title}</h1>
             <p className="lead">{description}</p>
           </div>
           <div className={className}>{children}</div>
