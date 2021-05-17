39cf5b2051b0f7b3942560ee93728d9e73aa1f17
AmarnathN <vayuteja@gmail.com>
2021-05-17 17:54:14 +0530

    Updated Admin dashboard to have Accordian

--- projfrontend/src/user/AdminDashboard.js
+++ projfrontend/src/user/AdminDashboard.js
@@ -2,31 +2,36 @@ import React from "react";
 import { isAuthenticated } from "../auth/helper";
 import Base from "../core/Base";
 import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 const AdminDashboard = () => {
   const { user } = isAuthenticated();
 
   const adminLeftPanel = () => {
     return (
-      <div className="card">
-        {/* <h4 className="card-header bg-dark bg-gradient text-white">Admin Navigation</h4> */}
-        <ul className="list-group">
-          <li className="list-group-item">
-            <Link to="/admin/categories" className="nav-link text-dark">
+      <div className="accordion" id="adminLeftAccordion">
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingOne">
+            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseCategories">
               Categories
-            </Link>
-          </li>
-          <li className="list-group-item">
-            <Link to="/admin/products" className="nav-link text-dark">
-              Products
-            </Link>
-          </li>
-          <li className="list-group-item">
-            <Link to="/admin/orders" className="nav-link text-dark">
-              Orders
-            </Link>
-          </li>
-        </ul>
+            </button>
+          </h2>
+          <div id="collapseCategories" className="accordion-collapse collapse" data-bs-parent="#adminLeftAccordion">
+            <ul className="list-group">
+              <li className="list-group-item">
+                <Link to="/admin/create/category" className="nav-link text-dark">
+                  Create Category
+                </Link>
+              </li>
+              <li className="list-group-item">
+                <Link to="/admin/manage/categories" className="nav-link text-dark">
+                  Manage Category
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   };
@@ -44,7 +49,7 @@ const AdminDashboard = () => {
           </li>
 
           <li className="list-group-item">
-            <span className="badge bg-danger"> Admin </span>
+            <span className="badge bg-danger"> Admin </span> {user.role}
           </li>
         </ul>
       </div>
@@ -54,8 +59,8 @@ const AdminDashboard = () => {
   return (
     <Base title="Admin Dashboard Page" description="Manage your shop here" className="container bg-secondary bg-gradient p-4">
       <div className="row">
-        <div className="col-3">{adminLeftPanel()}</div>
-        <div className="col-9">{adminRightPanel()}</div>
+        <div className="col-md-3 col-sm-12">{adminLeftPanel()}</div>
+        <div className="col-md-9 col-sm-12">{adminRightPanel()}</div>
       </div>
     </Base>
   );
