f472836bf79d149c8f35db610c9ca4dc9697f9b7
AmarnathN <vayuteja@gmail.com>
2021-05-17 22:34:25 +0530

    Fix for Nav Bar responsiveness

--- projfrontend/src/core/NavigationBar.js
+++ projfrontend/src/core/NavigationBar.js
@@ -8,62 +8,73 @@ const currentTab = (history, path) => {
 
 const NavigationBar = ({ history }) => {
   return (
-    <ul className="nav nav-tabs bg-dark card-header-tabs">
-      <li className="nav-item">
-        <Link style={currentTab(history, "/")} className="nav-link" to="/">
-          Home
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link style={currentTab(history, "/cart")} className="nav-link" to="/cart">
-          Cart
-        </Link>
-      </li>
-      {isAuthenticated() && isAuthenticated().user.role != 1 && (
-        <li className="nav-item">
-          <Link style={currentTab(history, "/user/dashboard")} className="nav-link" to="/user/dashboard">
-            Dashboard
-          </Link>
-        </li>
-      )}
-      {isAuthenticated() && isAuthenticated().user.role == 1 && (
-        <li className="nav-item">
-          <Link style={currentTab(history, "/admin/dashboard")} className="nav-link" to="/admin/dashboard">
-            Dashboard
-          </Link>
-        </li>
-      )}
-      {!isAuthenticated() && (
-        <React.Fragment>
-          <li className="nav-item">
-            <Link style={currentTab(history, "/signup")} className="nav-link" to="/signup">
-              Sign Up
-            </Link>
-          </li>
+    <nav class="navbar navbar-light navbar-expand-md">
+      <Link class="navbar-brand text-warning display-12" to="/">
+        <strong>{process.env.SHOPNAME || "My Shop"}</strong>
+      </Link>
+      <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#BasenavbarToggler">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
+      <div class="collapse navbar-collapse" id="BasenavbarToggler">
+        <ul className="navbar-nav card-header-tabs">
           <li className="nav-item">
-            <Link style={currentTab(history, "/signin")} className="nav-link" to="/signin">
-              Sign In
+            <Link style={currentTab(history, "/")} className="nav-link" to="/">
+              Home
             </Link>
           </li>
-        </React.Fragment>
-      )}
-      {isAuthenticated() && (
-        <li className="nav-item">
-          <span
-            className="nav-link"
-            style={currentTab(history, "/signin")}
-            onClick={() => {
-              signout(() => {
-                history.push("/signin");
-              });
-            }}
-          >
-            Signout
-          </span>
-        </li>
-      )}
-    </ul>
+          <li className="nav-item">
+            <Link style={currentTab(history, "/cart")} className="nav-link" to="/cart">
+              Cart
+            </Link>
+          </li>
+          {isAuthenticated() && isAuthenticated().user.role != 1 && (
+            <li className="nav-item">
+              <Link style={currentTab(history, "/user/dashboard")} className="nav-link" to="/user/dashboard">
+                Dashboard
+              </Link>
+            </li>
+          )}
+          {isAuthenticated() && isAuthenticated().user.role == 1 && (
+            <li className="nav-item">
+              <Link style={currentTab(history, "/admin/dashboard")} className="nav-link" to="/admin/dashboard">
+                Dashboard
+              </Link>
+            </li>
+          )}
+          {!isAuthenticated() && (
+            <React.Fragment>
+              <li className="nav-item">
+                <Link style={currentTab(history, "/signup")} className="nav-link" to="/signup">
+                  Sign Up
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link style={currentTab(history, "/signin")} className="nav-link" to="/signin">
+                  Sign In
+                </Link>
+              </li>
+            </React.Fragment>
+          )}
+          {isAuthenticated() && (
+            <li className="nav-item">
+              <span
+                className="nav-link"
+                style={currentTab(history, "/signin")}
+                onClick={() => {
+                  signout(() => {
+                    history.push("/signin");
+                  });
+                }}
+              >
+                Signout
+              </span>
+            </li>
+          )}
+        </ul>
+      </div>
+    </nav>
   );
 };
 
