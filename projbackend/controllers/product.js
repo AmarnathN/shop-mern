01453cf4d8c182689a7f2631a9784701659ccab9
AmarnathN <vayuteja@gmail.com>
2021-05-18 11:28:21 +0530

    Fix Error key from Err to maintian unformity

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -11,9 +11,9 @@ const formOptions = {
 exports.getProductById = (req, res, next, id) => {
   Product.findById(id)
     .populate("category")
-    .exec((err, product) => {
-      if (err) {
-        return res.status(400).json({ message: "Error finding the product ", error: err });
+    .exec((error, product) => {
+      if (error) {
+        return res.status(400).json({ message: "Error finding the product ", error: error });
       }
       if (!product) {
         return res.status(404).json({ message: "No product found " });
@@ -29,9 +29,9 @@ exports.getAllProducts = (req, res) => {
   Product.find()
     .sort([[sortBy, -1]])
     .limit(limit)
-    .exec((err, products) => {
-      if (err) {
-        return res.status(400).json({ message: "Error finding Products", error: err });
+    .exec((error, products) => {
+      if (error) {
+        return res.status(400).json({ message: "Error finding Products", error: error });
       }
       if (products.length == 0) {
         return res.status(404).json({ message: "No Products found" });
@@ -48,22 +48,23 @@ exports.createProduct = async (req, res) => {
         .then((file) => {
           return file;
         })
-        .catch((err) => {
-          res.json({ message: "Error occured while trying to upload to S3 bucket", err });
+        .catch((error) => {
+          res.json({ message: "Error occured while trying to upload to S3 bucket", error });
         });
       req.body.image = file.Location;
     }
+    console.log(req.body);
     let newProduct = new Product(req.body);
     newProduct
       .save()
       .then((product) => {
         res.json(product);
       })
-      .catch((err) => {
-        res.status(422).json({ message: "Error occured while trying to save to DB", err: err.message });
+      .catch((error) => {
+        res.status(422).json({ message: "Error occured while trying to save to DB", error: error.message });
       });
-  } catch (err) {
-    res.status(500).json({ message: "Error occured while trying to save to DB", err: err.message });
+  } catch (error) {
+    res.status(500).json({ message: "Error occured while trying to save to DB", error: error.message });
   }
 };
 
@@ -79,16 +80,16 @@ exports.updateProduct = async (req, res) => {
         .then((file) => {
           return file;
         })
-        .catch((err) => {
-          res.json({ message: "Error occured while trying to upload to S3 bucket", err });
+        .catch((error) => {
+          res.json({ message: "Error occured while trying to upload to S3 bucket", error });
         });
       // this is to delete the old linked file from S3
       req.deleteFileName = req.product.image;
       if (req.deleteFileName) {
         await s3FileDelete(req, res)
           .then()
-          .catch((err) => {
-            res.json({ message: "Error occured while trying to deleting file from S3 bucket", err });
+          .catch((error) => {
+            res.json({ message: "Error occured while trying to deleting file from S3 bucket", error });
           });
       }
       req.body.image = file.Location;
@@ -98,20 +99,20 @@ exports.updateProduct = async (req, res) => {
       .then((product) => {
         res.json(product);
       })
-      .catch((err) => {
-        res.status(422).json({ message: "Error occured while trying to Update Product in DB", err: err.message });
+      .catch((error) => {
+        res.status(422).json({ message: "Error occured while trying to Update Product in DB", error: error.message });
       });
-  } catch (err) {
-    res.status(500).json({ message: "Error occured while trying to Update Product in DB", err: err.message });
+  } catch (error) {
+    res.status(500).json({ message: "Error occured while trying to Update Product in DB", error: error.message });
   }
 };
 
 exports.deleteProduct = (req, res) => {
   req.filePathPrefix = PRODUCT_IMAGES_PATH;
 
-  Product.deleteOne({ _id: req.product._id }, (err, product) => {
-    if (err) {
-      return res.status(400).json({ message: "Error deleting product", error: err });
+  Product.deleteOne({ _id: req.product._id }, (error, product) => {
+    if (error) {
+      return res.status(400).json({ message: "Error deleting product", error: error });
     }
     req.deleteFileName = req.product.image;
     if (req.deleteFileName) {
@@ -119,8 +120,8 @@ exports.deleteProduct = (req, res) => {
         .then((file) => {
           res.json({ message: "Deleted the Product", product: req.product });
         })
-        .catch((err) => {
-          res.json({ message: "Error occured while trying to delete product from S3 bucket", err });
+        .catch((error) => {
+          res.json({ message: "Error occured while trying to delete product from S3 bucket", error });
         });
     }
   });
@@ -135,9 +136,9 @@ exports.updateStock = (req, res, next) => {
       },
     };
   });
-  Product.bulkWrite(myOperations, {}, (err, products) => {
-    if (err) {
-      return res.status(400).json({ message: "Error in bulk writing the product", error: err });
+  Product.bulkWrite(myOperations, {}, (error, products) => {
+    if (error) {
+      return res.status(400).json({ message: "Error in bulk writing the product", error: error });
     }
     next();
   });
