727013ceb23673ec722bc67e8699f070fef19c0c
AmarnathN <vayuteja@gmail.com>
2021-05-24 12:03:50 +0530

    add order helper

---
+++ projfrontend/src/core/helper/orderHelper.js
@@ -0,0 +1,19 @@
+import { API } from "../../backend";
+
+const createOrder = (token, orderData) => {
+  return fetch(`${API}/order/create/`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "content-type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(orderData),
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((error) => {
+      console.log("Error modify Item  : " + error);
+    });
+};
