ee7f0b1ab32028bed057ecac328c0389c8941361
AmarnathN <vayuteja@gmail.com>
2021-06-03 15:21:40 +0530

    Enhance Tabs styling

--- projfrontend/src/components/ui/controls/Tabs.js
+++ projfrontend/src/components/ui/controls/Tabs.js
@@ -1,47 +1,63 @@
 import React from "react";
-import { Paper, Tabs as MuiTabs, Tab, Box } from "@material-ui/core";
+import { Paper, Tabs as MuiTabs, Tab, Box, makeStyles, AppBar } from "@material-ui/core";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
   return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-width-tab-${index}`}
-      {...other}
-    >
+    <div role="tabpanel" hidden={value !== index} id={`tabpanel-${index}`} aria-labelledby={`tab-${index}`} {...other}>
       {value === index && <Box p={3}>{children}</Box>}
     </div>
   );
 }
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    padding: theme.spacing(1),
+    width: "100%",
+    backgroundColor: theme.palette.background.dark,
+  },
+  appBar: {
+    background: theme.palette.background.white,
+    boxShadow: `0 3px 5px 2px rgba(0, 0, 0, .3)`,
+  },
+}));
+
 const Tabs = (props) => {
   const [value, setValue] = React.useState(0);
-  const { tabsList } = props;
-
+  const { tabsList, variant } = props;
+  const classes = useStyles();
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
-    <React.Fragment>
-      <MuiTabs variant="fullWidth" value={value} indicatorColor="primary" textColor="primary" onChange={handleChange}>
-        {tabsList.length > 0 &&
-          tabsList.map((tabItem) => {
-            return <Tab label={tabItem.label}></Tab>;
-          })}
-      </MuiTabs>
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.appBar}>
+        <MuiTabs
+          value={value}
+          indicatorColor="primary"
+          textColor="secondary"
+          onChange={handleChange}
+          variant={variant || "fullWidth"}
+          scrollButtons="on"
+        >
+          {tabsList.length > 0 &&
+            tabsList.map((tabItem) => {
+              return <Tab wrapped label={tabItem.label}></Tab>;
+            })}
+        </MuiTabs>
+      </AppBar>
       {tabsList.length > 0 &&
         tabsList.map((tabItem, i) => {
           return (
-            <TabPanel value={value} index={i}>
+            <TabPanel value={value} index={i} key={i}>
               {tabItem.component}
             </TabPanel>
           );
         })}
-    </React.Fragment>
+    </div>
   );
 };
 
