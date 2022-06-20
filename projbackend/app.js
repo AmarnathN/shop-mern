09d9317443561524dacf8401aeda25b6e2915a98
AmarnathN <vayuteja@gmail.com>
2022-06-20 12:44:47 +0530

    Add express-routes-catalogue

--- projbackend/app.js
+++ projbackend/app.js
@@ -15,6 +15,8 @@ const cartItemRoutes = require("./routes/cartItem");
 const razorpayRoutes = require("./routes/razorpay");
 const shippingAddressRoutes = require("./routes/shippingAddress");
 
+const routesList = require("express-routes-catalogue");
+
 require("custom-env").env(process.env.NODE_ENV);
 
 // DB Connection
@@ -48,6 +50,12 @@ app.use((err, req, res, next) => {
     res.status(401).json({ error: err.name + ": " + err.message });
   }
 });
+
+routesList.default.web(
+  app,
+  "/routes-list"
+);
+// routesList.default.terminal(app)
 
 //Starting the server
 const port = process.env.PORT || 8000;
