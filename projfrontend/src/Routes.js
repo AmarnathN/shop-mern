c495f0a94d31e1740e2e8bdddf58e65c61bbf845
AmarnathN <vayuteja@gmail.com>
2024-01-25 00:27:59 +0530

    add vercel analytics

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -6,8 +6,6 @@ import Home from "./core/home/Home";
 import Cart from "./core/cart/Cart";
 import AdminDashboard from "./user/AdminDashboard";
 import UserDashboard from "./user/UserDashboard";
-import Signin from "./user/Signin";
-import Signup from "./user/SignUp";
 import ManageCategory from "./admin/ManageCategory";
 import CreateCategory from "./admin/CreateCategory";
 import CreateProduct from "./admin/CreateProduct";
@@ -16,22 +14,60 @@ import UpdateProduct from "./admin/UpdateProduct";
 import UpdateCategory from "./admin/UpdateCategory";
 import Checkout from "./core/checkout/Checkout";
 import NotFoundPage from "./core/NotFoundPage";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Routes() {
   return (
     <BrowserRouter>
+      <Analytics />
       <Switch>
         <Route path="/" exact component={Home}></Route>
         <PrivateRoute path="/cart" exact component={Cart}></PrivateRoute>
-        <PrivateRoute path="/checkout/" exact component={Checkout}></PrivateRoute>
-        <AdminRoute path="/admin/dashboard" exact component={AdminDashboard}></AdminRoute>
-        <PrivateRoute path="/user/dashboard" exact component={UserDashboard}></PrivateRoute>
-        <AdminRoute path="/admin/create/category" exact component={CreateCategory}></AdminRoute>
-        <AdminRoute path="/admin/manage/categories" exact component={ManageCategory}></AdminRoute>
-        <AdminRoute path="/admin/create/product" exact component={CreateProduct}></AdminRoute>
-        <AdminRoute path="/admin/manage/products" exact component={ManageProduct}></AdminRoute>
-        <AdminRoute path="/admin/update/product/:productId" exact component={UpdateProduct}></AdminRoute>
-        <AdminRoute path="/admin/update/category/:categoryId" exact component={UpdateCategory}></AdminRoute>
+        <PrivateRoute
+          path="/checkout/"
+          exact
+          component={Checkout}
+        ></PrivateRoute>
+        <AdminRoute
+          path="/admin/dashboard"
+          exact
+          component={AdminDashboard}
+        ></AdminRoute>
+        <PrivateRoute
+          path="/user/dashboard"
+          exact
+          component={UserDashboard}
+        ></PrivateRoute>
+        <AdminRoute
+          path="/admin/create/category"
+          exact
+          component={CreateCategory}
+        ></AdminRoute>
+        <AdminRoute
+          path="/admin/manage/categories"
+          exact
+          component={ManageCategory}
+        ></AdminRoute>
+        <AdminRoute
+          path="/admin/create/product"
+          exact
+          component={CreateProduct}
+        ></AdminRoute>
+        <AdminRoute
+          path="/admin/manage/products"
+          exact
+          component={ManageProduct}
+        ></AdminRoute>
+        <AdminRoute
+          path="/admin/update/product/:productId"
+          exact
+          component={UpdateProduct}
+        ></AdminRoute>
+        <AdminRoute
+          path="/admin/update/category/:categoryId"
+          exact
+          component={UpdateCategory}
+        ></AdminRoute>
         <Route path="*" component={NotFoundPage} />
       </Switch>
     </BrowserRouter>
